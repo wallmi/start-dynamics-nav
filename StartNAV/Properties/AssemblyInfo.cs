0f66347d6094c0428695a168df66d15d2a3cb4b7
wywallmi <michael.wallner@bene.com>
2022-06-17 07:23:57 +0200

    Release Version 1.11

--- StartNAV/Properties/AssemblyInfo.cs
+++ StartNAV/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Windows;
 // indem Sie "*" wie unten gezeigt eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.10.0.0")]
-[assembly: AssemblyFileVersion("1.10.0.0")]
+[assembly: AssemblyFileVersion("1.11.0.0")]
